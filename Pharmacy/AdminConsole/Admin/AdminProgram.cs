72b04d00f864bdf99f87c6215f553a3948e66e3f
BorisHamota <62393364+BorisHamota@users.noreply.github.com>
2021-06-30 08:53:35 +0300

    Add files via upload

---
+++ Pharmacy/AdminConsole/Admin/AdminProgram.cs
@@ -0,0 +1,14 @@
+﻿using MyPharmacy.PL.AdminMenu.Impl;
+
+namespace AdminConsole.Admin
+{
+    class AdminProgram
+    {
+        static void Main(string[] args)
+        {
+            MainMenu menu = new MainMenu();
+            menu.ShowMainMenu();
+        }
+    }
+
+}
