9761493c5ad21853517da7b23d2815134263321a
BorisHamota <62393364+BorisHamota@users.noreply.github.com>
2021-06-30 09:04:59 +0300

    Add files via upload

---
+++ Pharmacy/MyPharmacy/DAL/Factories/MemoryFactory.cs
@@ -0,0 +1,24 @@
+﻿using MyPharmacy.DAL.Factories.Abstract;
+using MyPharmacy.DAL.Repositories.Abstract;
+using MyPharmacy.DAL.Repositories.Impl.MemoryImpl;
+
+namespace MyPharmacy.DAL.Factories.Impl.MemoryImpl
+{
+    public class MemoryFactory : IFactory
+    {
+        public ICartRepositoty GetCartRepository()
+        {
+            return new CartRepository();
+        }
+
+        public ICosmeticRepository GetCosmeticRepository()
+        {
+            return new CosmeticRepository();
+        }
+
+        public IMedicineRepository GetMedicineRepository()
+        {
+            return new MedicineRepository();
+        }
+    }
+}
