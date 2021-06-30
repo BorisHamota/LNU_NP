e6d8f1189d010e5d23f0869cdbfdcd004c9605df
BorisHamota <62393364+BorisHamota@users.noreply.github.com>
2021-06-30 08:48:19 +0300

    Add files via upload

---
+++ Pharmacy/MyPharmacy/DAL/Factories/FileFactory.cs
@@ -0,0 +1,24 @@
+﻿using MyPharmacy.DAL.Factories.Abstract;
+using MyPharmacy.DAL.Repositories.Abstract;
+using MyPharmacy.DAL.Repositories.Impl.FileImpl;
+
+namespace MyPharmacy.DAL.Factories.Impl.FileImpl
+{
+    public class FileFactory : IFactory
+    {
+        public ICartRepositoty GetCartRepository()
+        {
+            return new CartFileRepository();
+        }
+
+        public ICosmeticRepository GetCosmeticRepository()
+        {
+            return new CosmeticFileRepository();
+        }
+
+        public IMedicineRepository GetMedicineRepository()
+        {
+            return new MedicineFileRepository();
+        }
+    }
+}
---
+++ Pharmacy/MyPharmacy/DAL/Factories/IFactory.cs
@@ -0,0 +1,11 @@
+﻿using MyPharmacy.DAL.Repositories.Abstract;
+
+namespace MyPharmacy.DAL.Factories.Abstract
+{
+    public interface IFactory
+    {
+        ICartRepositoty GetCartRepository();
+        ICosmeticRepository GetCosmeticRepository();
+        IMedicineRepository GetMedicineRepository();
+    }
+}
